fbc77d52999d0db1edc7c2e153b30df690c4804b
hungngo04 <ngotienhung195@gmail.com>
2022-12-19 22:41:10 -0500

    sea

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const sea = new seaLane(canvas.width/2, canvas.width);
 const fish = new Fish(100, 100, 30, 50);
 fish.draw(ctx);
 
@@ -11,6 +12,7 @@ animate();
 function animate(){
     fish.update();
     canvas.height = window.innerHeight;
+    sea.draw(ctx);
     fish.draw(ctx);
     requestAnimationFrame(animate);
 }
